5e47a95b9a2e2e2bd8663895866e9d670aa65ba5
eandx <rvs@bisv.ru>
2017-11-19 20:36:17 +0300

    1.3 JavaDoc. Code convention

--- projects/chapter_001/src/ru/job4j/Calculate.java
+++ projects/chapter_001/src/ru/job4j/Calculate.java
@@ -1,6 +1,14 @@
 package ru.job4j;
 
+/**
+	* Класс Calculate выводит приветмир.
+**/
+
 public class Calculate{
+	
+	/** 
+		* Медом Calculate
+	**/
 	
 	public static void main(String [] args){
 		String s = "Hello word!"
---
+++ projects/chapter_001/src/ru/job4j/packege-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Vadim (xxx_Xxx_vadimka@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
